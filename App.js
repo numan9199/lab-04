44575b9a4c405503411c4cefb54b1e7ea52f7e25
numan9199 <6210110176@psu.ac.th.com>
2021-08-15 15:52:02 +0700

    2

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weathers';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
+ return (
+  <View style={styles.container}>
+  <Weather zipCode="90110"/>
+  <StatusBar style="auto" />
+  </View>
+ );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+ },
 });
